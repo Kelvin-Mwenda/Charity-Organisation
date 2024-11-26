ed5916b4d5917798369cb6defb7e9a59b8b88e70
Kelvin <kelvinmwendadoreen@gmail.com>
2024-11-26 11:14:54 +0300

    nav bar refresh solution...

--- src/components/pages/HeaderNav.jsx
+++ src/components/pages/HeaderNav.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from 'react-router-dom'
+//import { NavLink } from 'react-router-dom'
 import 'react-bootstrap'
 import 'bootstrap/scss/bootstrap.scss';
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -6,21 +6,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 export default function HeaderNav() {
     return (
         <>
-            <header className="sticky-top bg-light shadow-sm">
+            <header className="sticky-top bg-body shadow-sm">
                 <nav> 
-                    <NavLink to="/">Home</NavLink>
-                    <NavLink to="about">About</NavLink>
-                    <NavLink to="projects">Projects</NavLink>
-                    <NavLink to="plans">Campaigns</NavLink>
-                    <NavLink to="news">News</NavLink>
-                    <NavLink to="contact">Contact</NavLink>
-                    <NavLink to="donate">Donate</NavLink>
+                    <a href="/">Home</a>
+                    <a href="/about">About</a>
+                    <a href="/projects">Projects</a>
+                    <a href="/plans">Campaigns</a>
+                    <a href="/news">News</a>
+                    <a href="/contact">Contact</a>
+                    <a href="/donate">Donate</a>
                 </nav>
                 <div className="profile">
-                        <NavLink to="signup">Sign Up</NavLink>
-                        <NavLink to="login">Login</NavLink>
+                        <a href="signup">Sign Up</a>
+                        <a href="Login">Login</a>
                         <div>
-                            <NavLink to="profile" className="my-profile" style={{backgroundImage : 'url(../src/assets/father.jpg)'}}></NavLink>
+                            <a href="profile" className="my-profile" style={{backgroundImage : 'url(../src/assets/father.jpg)'}}></a>
                         </div>
                         
                     </div>
